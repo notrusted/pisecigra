6c38f728e9c7edf9f5644a8fc4e9482d5b319993
kukup <kukupoli@mail.ru>
2023-05-12 23:37:50 +0400

    доделка

--- file_for_class.py
+++ file_for_class.py
@@ -112,6 +112,8 @@ class Nazgul_boss(Boss):
 class BossOrkConqueror(Boss):
     def __init__(self, hp, armor, dmg, weapon, magic, cry, coord_x, coord_y, heal_boss):
         Boss.__init__(self, hp, armor, dmg, weapon, magic)
+        self.hp = hp
+        self.name = "BossOrkConqueror"
         self.cry = cry
         self.coord_x = coord_x
         self.coord_y = coord_y
@@ -120,6 +122,15 @@ class BossOrkConqueror(Boss):
         self.anim = 0
         self.flag_orc_cry = True
         self.flag_boss_to_heal = True
+        self.time = pygame.USEREVENT + 1
+        self.time_definition = False
+        self.time_to_protective_enable = pygame.USEREVENT + 1
+        self.time_to_protective_enable_DEFINITION = False
+        self.time_to_protective_unable = pygame.USEREVENT + 1
+        self.time_to_protective_unable_DEFINITION = False
+        self.flag_protective_dome_enable = False
+        self.flag_protective_dome_unable = True
+        self.can_protectiveDome = True
 
     def healing(self):
         if self.heal > 0:
